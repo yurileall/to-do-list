24c5d730b53e3ca1046e06a2922554193567f1f7
yuri leal <yurileal111@outlook.com>
2025-03-28 22:16:38 -0300

    colocando a funcionalide sair do sistema.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
 </body>
 
 <footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('assets/js/logout.js') }}"></script>
 </footer>
 
